d5e233f6974e98c6c822cd17d61c77178a97c047
Denis <tudow40@gmail.com>
2022-01-03 21:30:57 +0300

    comm

--- components/FilmsSearchList/ListUl.tsx
+++ components/FilmsSearchList/ListUl.tsx
@@ -3,65 +3,66 @@ import styles from './FilmsSearchList.module.scss'
 import Item from './Item';
 import { IListUlProps } from './types';
 
-const ListUl = React.forwardRef<HTMLUListElement, IListUlProps>(function ListUlForward(props: IListUlProps, ref) {
-    const getItems = (): JSX.Element[] => {
-        const items: JSX.Element[] = props.data.map((item, idx) => {
-            return <Item item={item} type={props.type} key={item.id}/>
-        })
+const ListUl = React.forwardRef<HTMLUListElement, IListUlProps>(
+    function forwListUl(props: IListUlProps, ref) {
+        const getItems = (): JSX.Element[] => {
+            const items: JSX.Element[] = props.data.map((item, idx) => {
+                return <Item item={item} type={props.type} key={item.id}/>
+            })
 
-        return items;
-    }
-
-    const defaultStyle = {
-        transition: `all ${props.animationDuration}ms ease-in-out`,
-        opacity: 0,
-    }
-
-    const transitionStyles: {
-        [key: string]: {
-            [key: string] : number | string
+            return items;
         }
-    } = {
-        entering: {
-            opacity: 1,
-            transform: "scale(.001)"
-        },
-        entered:  {
-            opacity: 1,
-            transform: "scale(1)",
-        },
-        exiting:  {
-            position: "absolute",
-            top: 0,
-            left: 0,
+
+        const defaultStyle = {
+            transition: `all ${props.animationDuration}ms ease-in-out`,
             opacity: 0,
-            transform: "scale(.001)"
-        },
-        exited:   {
-            position: "absolute",
-            top: 0,
-            left: 0,
-            opacity: 0
-        },
-    };
+        }
 
-    if (!props.data[0]) return <div className={styles.listContainer}>
-        <h2 style={{
-            ...defaultStyle,
-            ...transitionStyles[props.animationState]
-        }}>Ничего не найдено</h2>
-    </div>
+        const transitionStyles: {
+            [key: string]: {
+                [key: string] : number | string
+            }
+        } = {
+            entering: {
+                opacity: 1,
+                transform: "scale(.001)"
+            },
+            entered:  {
+                opacity: 1,
+                transform: "scale(1)",
+            },
+            exiting:  {
+                position: "absolute",
+                top: 0,
+                left: 0,
+                opacity: 0,
+                transform: "scale(.001)"
+            },
+            exited:   {
+                position: "absolute",
+                top: 0,
+                left: 0,
+                opacity: 0
+            },
+        };
 
-    return <div className={styles.listContainer}>
-            <ul className={styles.list} ref={ref} style={{
+        if (!props.data[0]) return <div className={styles.listContainer}>
+            <h2 style={{
                 ...defaultStyle,
                 ...transitionStyles[props.animationState]
-            }}>
-                { getItems() }
-            </ul>
+            }}>Ничего не найдено</h2>
         </div>
-    
-});
+
+        return <div className={styles.listContainer}>
+                <ul className={styles.list} ref={ref} style={{
+                    ...defaultStyle,
+                    ...transitionStyles[props.animationState]
+                }}>
+                    { getItems() }
+                </ul>
+        </div>
+    }
+);
 
 ListUl.displayName = 'ListUl';
 
